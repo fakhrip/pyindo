a2c4f9254f91a9ecb73daa2a5544ac279f53a34d
fakhrip <fakhrip@protonmail.com>
2022-07-28 22:54:14 +0700

    Add return value parser and error statement

--- main.py
+++ main.py
@@ -8,6 +8,13 @@ def help():
         \r
         \rwhere `input file` is a program written in pyindo language"""
     )
+    exit()
+
+
+def error(statement, line_number=None):
+    line_number_str = f" (on line number {line_number})" if line_number else ""
+    print(f"\rError: {statement}{line_number_str}")
+    exit()
 
 
 if __name__ == "__main__":
@@ -16,34 +23,48 @@ if __name__ == "__main__":
         f_input = argv[1]
     except:
         help()
-        exit()
 
     # Read the file input given in the first argument
     with open(f_input, "r") as f:
         f_buffer = f.read().split("\n")
 
         is_entrypoint_exist = False
+        return_value = 0
         bytecode = []
 
-        for line in f_buffer:
+        for line_num, line in enumerate(f_buffer):
             if "utama() {" in line:
                 is_entrypoint_exist = True
 
             if "tampilkan(" in (stripped_line := line.strip()):
                 bytecode.append(Instr("LOAD_NAME", "print"))
 
-                argument = stripped_line.replace("tampilkan(\"", "")
-                argument = argument.replace("\");", "")
+                argument = stripped_line.replace('tampilkan("', "")
+                argument = argument.replace('");', "")
                 argument = argument.strip()
 
                 bytecode.append(Instr("LOAD_CONST", argument))
                 bytecode.append(Instr("CALL_FUNCTION", 1))
 
-        bytecode.extend([
-            Instr("POP_TOP"), 
-            Instr("LOAD_CONST", None), 
-            Instr("RETURN_VALUE")
-        ])
+            if "return" in line:
+                try:
+                    return_value = line.replace("return", "")
+                    return_value = return_value.replace(";", "")
+                    return_value = int(return_value)
+                except:
+                    error(
+                        statement="Return value should be integer", 
+                        line_number=line_num
+                    )
+
+        if not is_entrypoint_exist:
+            error(
+                statement="Entrypoint is not exist, you should create it first using `utama()` function"
+            )
+
+        bytecode.extend(
+            [Instr("POP_TOP"), Instr("LOAD_CONST", return_value), Instr("RETURN_VALUE")]
+        )
 
         compiled_bytecode = Bytecode(bytecode)
         code = compiled_bytecode.to_code()

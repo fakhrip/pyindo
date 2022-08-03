81a992394dd8ebb41b81a06b6a4c2deac3762a59
fakhrip <fakhrip@protonmail.com>
2022-08-03 21:11:12 +0700

    feature: add test runner

--- Makefile
+++ Makefile
@@ -2,3 +2,6 @@
 
 install:
 	pip3 install -r requirements.txt
+
+test:
+	python3 test.py
---
+++ test.py
@@ -0,0 +1,40 @@
+from os import popen
+from glob import glob
+
+if __name__ == "__main__":
+    incorrect_programs, incorrect_programs_out = [glob("tests/error/*.pyind"), glob("tests/error/*.output")]
+    correct_programs, correct_programs_out = [glob("tests/success/*.pyind"), glob("tests/success/*.output")]
+
+    print("==============================")
+    print("[+] Testing incorrect programs")
+    print("==============================")
+
+    # Test all incorrect programs
+    for iteration, program, output in zip(range(len(incorrect_programs)), incorrect_programs, incorrect_programs_out):
+        program_out = popen(f"python3 main.py {program}").read().lstrip("\n")
+        expected_out = open(output, "r").read()
+
+        print(f"[{iteration + 1}] {program} ", end="")
+        if program_out == expected_out:
+            print("(PASSED)")
+        else:
+            print("(FAIL)")
+            print(f"Expected: `{expected_out}`")
+            print(f"Got: `{program_out}`", end="\n\n")
+
+    print("\n============================")
+    print("[+] Testing correct programs")
+    print("============================")
+
+    # Test all correct programs
+    for iteration, program, output in zip(range(len(correct_programs)), correct_programs, correct_programs_out):
+        program_out = popen(f"python3 main.py {program}").read().lstrip("\n")
+        expected_out = open(output, "r").read()
+
+        print(f"[{iteration + 1}] {program} ", end="")
+        if program_out == expected_out:
+            print("(PASSED)")
+        else:
+            print("(FAIL)")
+            print(f"Expected: `{expected_out}`")
+            print(f"Got: `{program_out}`", end="\n\n")
